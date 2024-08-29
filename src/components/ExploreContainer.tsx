27129f72f7553f331971789f02d779403bb0f179
unknown <Alumno@SCAAOB00LC60314.duoc.acad>
2024-08-29 14:27:00 -0400

    actualizacion 2

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -7,7 +7,7 @@ function ExploreContainer() {
     <IonCard>
       <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
       <IonCardHeader>
-        <IonCardTitle>Card Title</IonCardTitle>
+        <IonCardTitle>Card Titleeeeee</IonCardTitle>
         <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
       </IonCardHeader>
 
